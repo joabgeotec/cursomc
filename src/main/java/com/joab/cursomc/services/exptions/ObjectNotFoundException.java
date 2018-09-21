2eeae15e13b3cc732abd4d5dafa204ccadc1cfc0
Joab Brito <joabbrito7@gmail.com>
2018-09-21 16:23:42 -0300

    Ajustes no endpoint/categorias/{id}

---
+++ src/main/java/com/joab/cursomc/services/exptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.joab.cursomc.services.exptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+}
